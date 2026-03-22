803484565ad90c6197768d2ac49b7f0f42cd3c37
Vaibhav Patil <vaibhavvpatil132@gmail.com>
2026-03-22 12:36:47 +0530

    created navbar and header

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex justify-between items-center p-5 px-7 sm:p-6 sm:px-24 absolute top-0">
+      <div className="flex gap-2 items-center">
+        <img src={assets.mylogo} alt="" className="w-9 sm:w-12" />
+        <h1 className="text-2xl font-bold sm:text-3xl">Auth</h1>
+      </div>
+      <button className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100">
+        Login <img src={assets.arrow_icon} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
