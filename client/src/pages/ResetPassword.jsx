e19ef90a6cffc4a0fa2c9d19f8d22f7aafbc99e4
Vaibhav Patil <vaibhavvpatil132@gmail.com>
2026-03-22 18:38:12 +0530

    added the email verification functionality

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,9 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const inputRefs = React.useRef([]);
+
+  const handleInput = (e, index) => {
+    if (e.target.value.length > 0 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  const handleKeyDown = (e, index) => {
+    if (e.key === "Backspace" && e.target.value === "" && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
+  };
+
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData("text");
+    const pasteArray = paste.split("");
+    pasteArray.forEach((char, index) => {
+      if (inputRefs.current[index]) {
+        inputRefs.current[index].value = char;
+      }
+    });
+  };
   return (
-    <div>
-      <h1>Reset Password Page</h1>
+    <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-blue-200 to-blue-400">
+      <div
+        onClick={() => navigate("/")}
+        className="flex gap-2 items-center absolute left-7 sm:left-24 top-6 w-28 sm:w-32"
+      >
+        <img src={assets.mylogo} alt="" className="w-9 sm:w-12" />
+        <h1 className="text-2xl font-bold sm:text-3xl">Auth</h1>
+      </div>
+
+      {/* Email input form */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password
+        </h1>
+        <p className="text-center mb-6 text-blue-300">
+          Enter your registered email address
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <img src={assets.mail_icon} alt="" className="w-3 h-3" />
+          <input
+            type="email"
+            placeholder="Email id"
+            className="bg-transparent outline-none text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 bg-linear-to-r from-blue-500 to-blue-900 text-white rounded-full mt-3">
+          Submit
+        </button>
+      </form>
+
+      {/* OTP input form */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          Reset Password OTP
+        </h1>
+        <p className="text-center mb-6 text-blue-300">
+          Enter the 6-digit code sent to your email id.
+        </p>
+        <div className="flex justify-between mb-8" onPaste={handlePaste}>
+          {Array(6)
+            .fill(0)
+            .map((_, index) => (
+              <input
+                type="text"
+                maxLength="1"
+                key={index}
+                required
+                className="w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md"
+                ref={(e) => (inputRefs.current[index] = e)}
+                onInput={(e) => handleInput(e, index)}
+                onKeyDown={(e) => handleKeyDown(e, index)}
+              />
+            ))}
+        </div>
+        <button className="w-full py-2.5 bg-linear-to-r from-blue-500 to-blue-900 text-white rounded-full">
+          Submit
+        </button>
+      </form>
+
+      {/* Enter new passord */}
+      <form className="bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm">
+        <h1 className="text-white text-2xl font-semibold text-center mb-4">
+          New Password
+        </h1>
+        <p className="text-center mb-6 text-blue-300">
+          Enter the new passord below
+        </p>
+        <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+          <img src={assets.mail_icon} alt="" className="w-3 h-3" />
+          <input
+            type="email"
+            placeholder="Email id"
+            className="bg-transparent outline-none text-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <button className="w-full py-2.5 bg-linear-to-r from-blue-500 to-blue-900 text-white rounded-full mt-3">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
