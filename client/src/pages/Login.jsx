3291de80c3179c0f12e908baa1eb737abe87287b
Vaibhav Patil <vaibhavvpatil132@gmail.com>
2026-03-22 16:00:42 +0530

    created login page and context provider

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-linear-to-br from-blue-200 to-blue-400">
+      <div
+        onClick={() => navigate("/")}
+        className="flex gap-2 items-center absolute left-7 sm:left-24 top-6 w-28 sm:w-32"
+      >
+        <img src={assets.mylogo} alt="" className="w-9 sm:w-12" />
+        <h1 className="text-2xl font-bold sm:text-3xl">Auth</h1>
+      </div>
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-blue-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </h2>
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
+
+        <form>
+          {state === "Sign Up" && (
+            <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+              <img src={assets.person_icon} alt="" />
+              <input
+                type="text"
+                className="bg-transparent outline-none"
+                placeholder="Full Name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                required
+              />
+            </div>
+          )}
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="" />
+            <input
+              type="email"
+              className="bg-transparent outline-none"
+              placeholder="Email Id"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+          </div>
+
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.lock_icon} alt="" />
+            <input
+              type="password"
+              className="bg-transparent outline-none"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </div>
+
+          <p
+            onClick={() => navigate("/reset-password")}
+            className="mb-4 text-blue-500 cursor-pointer"
+          >
+            Forgot password
+          </p>
+
+          <button className="w-full py-2.5 rounded-full bg-linear-to-r from-blue-500 to-blue-900 text-white font-medium">
+            {state}
+          </button>
+        </form>
+
+        {state === "Sign Up" ? (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className="text-blue-400 cursor-pointer underline"
+            >
+              Sign up
+            </span>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
